7199cf1d80ca58aaa05e28a1819024b0f5234e76
hyesung <hshs321@naver.com>
2023-09-14 16:34:13 +0900

    [Improvement] weather

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ import json
 from dotenv import load_dotenv
 import os
 from datetime import datetime, timedelta
+import cachetools
+
 app = FastAPI()
+
+cache = cachetools.TTLCache(maxsize=100, ttl=3600)
 
 origins = [
     "http://localhost",
@@ -73,30 +77,42 @@ async def getBusInfo(bus_no: str):
 
 
 @app.get("/api/weather/nx={nx}&ny={ny}")
+# @cachetools.cached(cache)
 async def getWeather(nx: int, ny: int):
     # Get current time
-    now = datetime.now() - timedelta(minutes=15)
+    now = datetime.now()
 
-    # Get base_date and base_time
-    if now.hour == 23 and now.minute > 30:
-        # If it's after 23:30, set base_date to the next day
-        base_date = (now + timedelta(days=1)).strftime("%Y%m%d")
-        base_time = "0000"
+    # Calculate base_date and base_time
+    if now.minute <= 30:
+        base_time = f"{now.hour - 1:02}00"
     else:
-        base_date = now.strftime("%Y%m%d")
-        minute = "00" if now.minute <= 30 else "30"
-        base_time = f"{now.hour:02}{minute}"
+        base_time = f"{now.hour:02}00"
 
-    URL = f"https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey={API_KEY}&pageNo=1&numOfRows=100&dataType=JSON&base_date={base_date}&base_time={base_time}&nx={nx}&ny={ny}"
+    if now.hour == 0 and now.minute <= 30:  # Edge case for midnight
+        now = now - timedelta(days=1)  # Subtract a day to get the previous day's date
+
+    base_date = now.strftime("%Y%m%d")
+
+    URL = f"https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey={API_KEY}&pageNo=1&numOfRows=100&dataType=JSON&base_date={base_date}&base_time={base_time}&nx={nx}&ny={ny}"
+    print(URL)
 
     r = requests.get(URL)
     if r.status_code == 200:
         response_data = r.json()  # Parse the JSON response
+        print(response_data)
+        print(URL)
         items = response_data["response"]["body"]["items"]
         return items
     else:
         print(f"Received unexpected status code {r.status_code}: {r.text}")
         raise HTTPException(status_code=r.status_code, detail="Failed to fetch data from server")
+
+@app.get("/debug/cache")
+def view_cache():
+    cache_contents = dict(cache)
+    for key, value in cache_contents.items():
+        print(f"Key: {key}, Value: {value}")
+    return {"status": "Cache contents printed to console"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
